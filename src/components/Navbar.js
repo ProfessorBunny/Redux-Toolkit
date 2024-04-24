f5079503a54491ad6cbdfe5b601a64757859d459
Nikunj <nikunjraghav24@gmail.com>
2024-04-24 20:06:57 +0100

    setting up navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import { CartIcon } from "../icons";
+import { useSelector } from "react-redux";
+
+import React from 'react'
+
+const Navbar = () => {
+
+    return (
+        <nav className="nav-center">
+            <div>
+                <h3>Redux Toolkit</h3>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
