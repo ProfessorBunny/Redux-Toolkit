c6843a1c233780e2d7cf89cfd89cc4f942f9881d
Nikunj <nikunjraghav24@gmail.com>
2024-05-07 14:36:45 +0100

    fetching data asynchronously from API

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import cartItems from '../../cartItems'
 
 const initialState = {
-    cartItems: cartItems,
+    cartItems: [],
     amount: 1,
     total: 0,
     isLoading: true,
