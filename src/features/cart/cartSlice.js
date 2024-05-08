5bced6f71f55bcfeb736fefe5ccc2f8742935287
Nikunj <nikunjraghav24@gmail.com>
2024-05-08 13:14:50 +0100

    switching fetch with axios

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,5 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import cartItems from '../../cartItems'
+import axios from "axios";
 
 const initialState = {
     cartItems: [],
@@ -11,10 +12,13 @@ const initialState = {
 const url = 'https://www.course-api.com/react-useReducer-cart-project';
 
 
-export const getCartItems = createAsyncThunk('cart/getCartItems', () => {
-    return fetch(url)
-        .then((resp) => resp.json())
-        .catch((err) => console.log(err));
+export const getCartItems = createAsyncThunk('cart/getCartItems', async () => {
+    try {
+        const resp = await axios(url)
+        return resp.data
+    } catch (error) {
+
+    }
 });
 
 
